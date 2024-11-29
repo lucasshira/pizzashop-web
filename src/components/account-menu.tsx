1237a3502d29dd7925ad39ec4c9aaf71ed129670
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-11-29 12:13:40 -0300

    feat: add header and nav menu

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,44 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Button } from "./ui/button";
+import { Building, ChevronDown, LogOut } from "lucide-react";
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Pizza Shop
+          <ChevronDown className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Lucas Gabriel</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            lucas.shirabayashi@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="mt-2 flex cursor-pointer items-center">
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da Loja</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="mt-2 flex cursor-pointer items-center text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
