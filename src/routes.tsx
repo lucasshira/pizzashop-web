cb63d1ef414fa488e80302d48861cd598d4d7a02
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-12-02 12:24:32 -0300

    feat: add table list in orders

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,12 +4,16 @@ import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignUp } from "./pages/auth/sign-up";
+import Orders from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "/", element: <Dashboard /> }],
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+    ],
   },
   {
     path: "/",
