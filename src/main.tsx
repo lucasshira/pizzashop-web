ac6d11ce36301e6d4f3c66522ccbd461e950de6f
Lucas Azevedo <lucas.shirabayashi@gmail.com>
2025-02-23 19:33:38 +0000

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { enableMSW } from "./api/mocks";
-import { App } from "./app";
+import { App } from "./App";
+
 
 enableMSW().then(() => {
   createRoot(document.getElementById("root")!).render(
